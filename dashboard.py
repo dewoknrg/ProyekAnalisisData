5e9e9e537b0ccf00bb3c57f3177df46ef691e0a2
dewoknrg <150086648+dewoknrg@users.noreply.github.com>
2023-11-07 05:57:05 +0700

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -114,7 +114,31 @@ st.title('Proyek Analisis Data :sparkles:')
 st.write('''"Proyek ini disusun sebagai syarat untuk menyelesaikan kelas Belajar Analisis Data dengan Python Dicoding"''')
 
 st.header('Bike Sharing Dataset')
-st.dataframe(data=bike_sharing, width=500, height=500)
+st.subheader('Raw Dataset hour.csv')
+st.dataframe(data=hour, width=500, height=300)
+st.subheader('Raw Dataset day.csv')
+st.dataframe(data=day, width=500, height=300)
+st.write('''- instant: record index
+	- dteday : date
+	- season : season (1:springer, 2:summer, 3:fall, 4:winter)
+	- yr : year (0: 2011, 1:2012)
+	- mnth : month ( 1 to 12)
+	- hr : hour (0 to 23)
+	- holiday : weather day is holiday or not (extracted from http://dchr.dc.gov/page/holiday-schedule)
+	- weekday : day of the week
+	- workingday : if day is neither weekend nor holiday is 1, otherwise is 0.
+	+ weathersit : 
+		- 1: Clear, Few clouds, Partly cloudy, Partly cloudy
+		- 2: Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds, Mist
+		- 3: Light Snow, Light Rain + Thunderstorm + Scattered clouds, Light Rain + Scattered clouds
+		- 4: Heavy Rain + Ice Pallets + Thunderstorm + Mist, Snow + Fog
+	- temp : Normalized temperature in Celsius. The values are divided to 41 (max)
+	- atemp: Normalized feeling temperature in Celsius. The values are divided to 50 (max)
+	- hum: Normalized humidity. The values are divided to 100 (max)
+	- windspeed: Normalized wind speed. The values are divided to 67 (max)
+	- casual: count of casual users
+	- registered: count of registered users
+	- cnt: count of total rental bikes including both casual and registered''')
 
 st.subheader('Pertanyaan Bisnis?')
 st.write('''- Bagaimana Pengaruh Musim Terhadap Jumlah Peminjaman Sepeda?
